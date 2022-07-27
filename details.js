97e0d8429fa2fa2185dd328ea64ac18de6e76f7a
Risbel Suarez Rodriguez <risbel961019@gmail.com>
2022-07-27 04:42:09 +0200

    cambios para pull

--- details.js
+++ details.js
@@ -1,12 +1,13 @@
 // Logica referente a details.html
 
-const fillPageData = (data) => {
-  console.log({ data });
+const fillPageData = (pokeData) => {
+  console.log(pokeData);
 
   const header = document.getElementById("details-header");
   header.innerHTML = `This is the details page for ${data.name}`;
 
   document.title = `Details for ${data.name}`;
+
 };
 
 window.onload = () => {
@@ -14,7 +15,9 @@ window.onload = () => {
   const params = new URLSearchParams(window.location.search);
 
   // Aquí no estabas haciendo el fetch
+   fetch(`https://pokeapi.co/api/v2/pokemon?name=${input}`)
   const pokemonName = params.get("name");
+ 
 
   srcPokemonByName(pokemonName).then((data) => fillPageData(data));
 };
